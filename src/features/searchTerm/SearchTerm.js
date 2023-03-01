a979e505cab04b2851066038fef4455d86cb809d
Alvaro-LGPA <alvarolaura@gmail.com>
2023-03-01 13:53:56 +0100

    Assign dispatch reference, searchTerm reference, remove props assignment and import selectSearchTerm

--- src/features/searchTerm/SearchTerm.js
+++ src/features/searchTerm/SearchTerm.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useSelector, useDispatch} from 'react-redux'
 
-import { setSearchTerm, clearSearchTerm } from './searchTermSlice.js';
+import { setSearchTerm, clearSearchTerm, selectSearchTerm } from './searchTermSlice.js';
 const searchIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/search.svg'
 const clearIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/clear.svg'
 
 export const SearchTerm = (props) => {
   
-  const { searchTerm, dispatch } = props;
+  
+
+  const dispatch = useDispatch();
+  const searchTerm = useSelector(selectSearchTerm)
+
 
   const onSearchTermChangeHandler = (e) => {
     const userInput = e.target.value;
