5d229cb728c41f68513afbb84688789e7d1af7e1
Alvaro-LGPA <alvarolaura@gmail.com>
2023-03-01 10:09:50 +0100

    Commented redundant actions

--- src/features/allRecipes/allRecipesSlice.js
+++ src/features/allRecipes/allRecipesSlice.js
@@ -1,17 +1,22 @@
 import allRecipesData from '../../data.js'
 
-export const loadData = () => {
-  return {
-    type: 'allRecipes/loadData',
-    payload: allRecipesData
-  }
-}
+
+// onLoad() not needed anymore. State has been initialized with initialstate=allRecipesData
+// export const loadData = () => {
+//   return {
+//     type: 'allRecipes/loadData',
+//     payload: allRecipesData
+//   }
+// }
 
 const initialState = allRecipesData; // BRANCH REMOVING USE EFFECT!!: initializing state so useEffect is not needed
 export const allRecipesReducer = (allRecipes = initialState, action) => {
   switch (action.type) {
-    case 'allRecipes/loadData': // esto carga todas las recetas en useEffect.
-      return action.payload;
+    
+    // action not needed anymore. State has been initialized with initialstate=allRecipesData
+    // case 'allRecipes/loadData': 
+    //   return action.payload;
+    
     case 'favoriteRecipes/addRecipe': // esto retira la receta que ha sido añadida a favoritos. Esta acción se despacha al mismo tiempo a allRecipesSlice y a favoriteRecipesSlice con diferentes retornos.
       return allRecipes.filter(recipe => recipe.id !== action.payload.id);
     case 'favoriteRecipes/removeRecipe': // esto devuelve la receta que esta en favoritos a allrecipes. Esta acción se despacha al mismo tiempo a allRecipesSlice y a favoriteRecipesSlice con diferentes retornos
