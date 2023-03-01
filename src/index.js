a10b6c03c157fd9eb11f197fd800ed61b06fdc20
Alvaro-LGPA <alvarolaura@gmail.com>
2023-03-01 10:42:53 +0100

    Refactors react Root API to update to createRoot

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import { createRoot } from  'react-dom/client'
 
 import { App } from './app/App.js';
 import { store } from './app/store.js';
 
+
+
+const container = document.getElementById('app');
+const root = createRoot(container)
 const render = () => {
-  ReactDOM.render(
+  root.render(
     <App 
       state={store.getState()}
       dispatch={store.dispatch}
-    />,
-    document.getElementById('root')
+    />
   )
 }
 store.subscribe(render);
